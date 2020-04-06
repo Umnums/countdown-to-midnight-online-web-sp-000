51fe135b0c4fd757a4b6e08e8fe2b6826cb2eba6
Joseph Kao <josephtkao@gmail.com>
2020-04-06 21:15:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,14 @@ def countdown(sec)
   until sec == 0
     puts "#{sec} SECOND(S)!"
     sec -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(sec)
+  until sec == 0
+    puts "#{sec} SECOND(S)!"
+    sec -= 1
     sleep(1)
   end
   "HAPPY NEW YEAR!"
